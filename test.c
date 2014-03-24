a6befbd618a238679a8bdd426e63a3984b5d55fe
noru <noru@noru-12.(none)>
2014-03-24 12:12:58 +0900

    add printf arr line

--- test.c
+++ test.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#define MAX 6
+int arr[MAX] ={5,10,15,20,25,30};
 
 int main()
 {
+    int i;
+
+    for (i=0; i < MAX; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+
 	printf("Hello World\n");
 	return 0;
 }
